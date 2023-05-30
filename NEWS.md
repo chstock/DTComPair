b7d34105725910543a43047ce6aec1ee95312436
Stock,Dr.,Christian (MED BDS) BIP-DE-I <christian.stock@boehringer-ingelheim.com>
2023-05-30 20:44:18 +0000

    Added function sesp.gen.mcnemar

--- NEWS.md
+++ NEWS.md
@@ -1,9 +1,10 @@
-## DTComPair version 1.2.1 (2023-05-08)
+## DTComPair version 1.2.2 (2023-05-30)
 
 - Functions that compare sensitivity/ specificity and predictive values now return lists of vectors (instead of lists of lists).
 - `Test 1` is consistently used as the reference test.
 - In `pv.gs`and `pv.wgs`, it is now `diff.ppv <- ppv.2-ppv.1` (instead of `diff.ppv <- abs(ppv.1-ppv.2)`), and accordingly for negative predictive values.
-- Added new function (`pv.prev`) to allow computation of positive and negative predictive values for different theoretical prevalances.
+- Added a new function (`pv.prev`) to allow computation of positive and negative predictive values for different theoretical prevalances.
+- Added a new function (`sesp.gen.mcnemar`) implementing a generalized McNemar's test for a joint comparison of sensitivity and specificity.
 
 ## DTComPair version 1.2.0 (2023-04-20)
 
---
+++ R/sesp.gen.mcnemar.R
@@ -0,0 +1,39 @@
+#' Generalized McNemar's test
+#'
+#' Performs a generalized McNemar's test to jointly compare sensitivity and specificity.
+#'
+#' @param tab An object of class `tab.paired`.
+#'
+#' @return A vector containing the test statistic and the p-value.
+#' 
+#' @export
+#' 
+#' @references Lachenbruch P.A., Lynch C.J. (1998). Assessing screening tests: extensions of McNemar's test. \emph{Stat Med}, 17(19): 2207-17.
+#' 
+#' @seealso [DTComPair::tab.paired()], [DTComPair::read.tab.paired()] and [DTComPair::sesp.mcnemar()]
+#'
+#' @examples
+#' # Example 1:
+#' data(Paired1) # Hypothetical study data
+#' ftable(Paired1)
+#' paired.layout1 <- tab.paired(d=d, y1=y1, y2=y2, data=Paired1)
+#' print(paired.layout1)
+#' sesp.gen.mcnemar(paired.layout1)
+#' 
+#' # Example 2 (from Lachenbruch and Lynch (1998)):
+#' paired.layout2 <- read.tab.paired(
+#'   d.a  = 850, d.b  = 40, d.c  = 60, d.d  =  50, 
+#'   nd.a =  60, nd.b = 25, nd.c = 15, nd.d = 900, 
+#'   testnames = c("T1", "T2")
+#' )
+#' print(paired.layout2)
+#' sesp.gen.mcnemar(paired.layout2)
+#' 
+sesp.gen.mcnemar <- function(tab) {
+  mcnemar <- sesp.mcnemar(tab)
+  X2 <- mcnemar$sensitivity["test.statistic"] + mcnemar$specificity["test.statistic"]
+  p.value <- 1 - pchisq(X2, df = 2)
+  results <- c(X2, p.value)
+  names(results) <- c("test.statistic", "p.value")
+  return(results)
+}
